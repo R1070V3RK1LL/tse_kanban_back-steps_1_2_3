3eb348943490c40b666fa1cbe590d513a9260ddf
berard.teo <teo99@live.fr>
2021-11-24 15:08:51 +0100

    controllers

--- src/main/java/org/rygn/kanban/controllers/DeveloperController.java
+++ src/main/java/org/rygn/kanban/controllers/DeveloperController.java
@@ -2,35 +2,38 @@ package org.rygn.kanban.controllers;
 
 import org.rygn.kanban.dao.DeveloperRepository;
 import org.rygn.kanban.domain.Developer;
+import org.rygn.kanban.service.DeveloperService;
+import org.rygn.kanban.service.impl.DeveloperServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("developers-rest")
+@RequestMapping("/developers-rest")
 public class DeveloperController {
+
+     @Autowired
+     private DeveloperService developerService;
+     private DeveloperRepository developerRepository;
 
      @GetMapping(path= "/{id}", produces = "application/json")
      public Optional<Developer> getDeveloper(@PathVariable long id){
           return findDeveloperById(id);
      }
-     @Autowired
-     private DeveloperRepository developerRepository;
 
      private Optional<Developer> findDeveloperById(long id) {
-          return developerRepository.findById(id);
+          return this.developerRepository.findById(id);
      }
      @GetMapping(path= "/developers", produces = "application/json")
      public List<Developer> getDevelopers(){
-          return findAllDevelopers();
+          return this.developerService.findAllDevelopers();
      }
-     public List<Developer> findAllDevelopers() {
 
-          return this.developerRepository.findAll();
+     @PostMapping(path= "/developers", consumes="application/json", produces= "application/json")
+     public Developer createDeveloper(Developer developer){
+          return this.developerRepository.save(developer);
      }
+
 }
--- src/main/java/org/rygn/kanban/controllers/TaskController.java
+++ src/main/java/org/rygn/kanban/controllers/TaskController.java
@@ -2,38 +2,43 @@ package org.rygn.kanban.controllers;
 
 
 import org.rygn.kanban.dao.TaskRepository;
-import org.rygn.kanban.domain.Developer;
 import org.rygn.kanban.domain.Task;
+import org.rygn.kanban.domain.TaskStatus;
+import org.rygn.kanban.service.TaskService;
+import org.rygn.kanban.service.impl.TaskServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("tasks-rest")
+@RequestMapping("/tasks-rest")
 public class TaskController {
 
-    @GetMapping(path= "/{id}", produces = "application/json")
-    public Optional<Task> getTask(@PathVariable long id){
-        return findTaskById(id);
-    }
     @Autowired
-    private TaskRepository taskRepository;
+    private TaskService taskService;
 
-    private Optional<Task> findTaskById(long id) {
-        return taskRepository.findById(id);
+    @GetMapping(path= "/{id}", produces = "application/json")
+    public Task getTask(@PathVariable long id){
+        return this.taskService.findTask(id);
     }
 
     @GetMapping(path= "/tasks", produces = "application/json")
-    public List<Task> getTasks(){
-        return findAllTasks();
+    public Collection<Task> getTasks(){
+        return this.taskService.findAllTasks();
     }
-    public List<Task> findAllTasks() {
 
-        return this.taskRepository.findAll();
+    
+    @PostMapping(path= "/tasks", consumes="application/json", produces= "application/json")
+    public Task createTask(Task task){
+        return this.taskService.createTask(task);
     }
+
+    @PatchMapping(path="tasks/{id}", consumes="application/json", produces="application/json")
+    public Task moveTask(Task task){
+        return this.taskService.moveRightTask(task);
+    }
+
 }
